e5d4264003f1643a13c6873a2a2c5ef08f123458
DeepikaRajarethinam <47515962+DeepikaRajarethinam@users.noreply.github.com>
2019-02-11 21:06:51 +0530

    Add files via upload

---
+++ PrintHello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class PrintHello{
+public static void main(String[]qwe){
+Scanner scan =new Scanner(System.in);
+int n=scan.nextInt();
+int m=1;
+while(m<=n)
+{
+System.out.println("HELLO");
+m++;
+}
+}
+}
