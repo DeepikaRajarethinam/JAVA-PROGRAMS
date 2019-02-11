710cbbc6dcab688d4a23d17cfd79c0313f820064
DeepikaRajarethinam <47515962+DeepikaRajarethinam@users.noreply.github.com>
2019-02-11 21:03:04 +0530

    Add files via upload

---
+++ CheckAlpha.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class CheckAlpha {
+public static void main(String[]qwe){
+Scanner scan =new Scanner (System.in);
+char ch=scan.next().charAt(0);
+if(Character.isLetter(ch))
+System .out.print("alpha");
+else
+System .out.print("nope");
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
